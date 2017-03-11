e6c552f1819b76f61354a7ced7a4c3f434945b48
poonamob <poonamob@gmail.com>
2017-03-11 23:37:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function(req,res)
 {
- res.send('You have hit the page to display article one!');   
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
